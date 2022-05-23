5e54db35a0915ebb8661eadf4008b7c8d41a7fc8
Thiago-M-Braga <thiago_braga@estudante.sesisenai.org.br>
2022-05-23 16:50:25 -0300

    q
    q

--- copianetflix/src/Tmdb.js
+++ copianetflix/src/Tmdb.js
@@ -1,2 +1,34 @@
 const API_KEY = '50fc67e44c2d72dd1c5af33c1287adf6';
 const API_BASE = 'https://api.themoviedb.org/3';
+
+export default {
+    getHomeList: async() => {
+        return [
+            {
+                slug:'originals',
+                title: 'Originais da Netflix',
+                items: []
+            },
+            {
+                slug:'trending',
+                title: 'Recomendados para você',
+                items: []
+            },
+            {
+                slug:'trending',
+                title: 'Recomendados para você',
+                items: []
+            },
+            {
+                slug:'trending',
+                title: 'Recomendados para você',
+                items: []
+            },
+            {
+                slug:'trending',
+                title: 'Recomendados para você',
+                items: []
+            },
+        ]
+    }
+}
