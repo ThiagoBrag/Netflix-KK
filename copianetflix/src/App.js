58032bd2c57fce32075063d539446570fd95f2d7
Thiago-M-Braga <thiago_braga@estudante.sesisenai.org.br>
2022-05-23 21:32:46 -0300

    aaa
    aa

--- copianetflix/src/App.js
+++ copianetflix/src/App.js
@@ -1,9 +1,38 @@
-import react from 'react';
+import react, { useEffect, useState } from 'react';
+import './App.css'
+import Tmdb from './Tmdb';
+import MovieRow from './components/movieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
-  return(
-    <div className="Container">
-      NETFLIX
+
+  const [movieList, setMovieList] = useState([]);
+  const [FeaturedData, setFeaturedData] = useState(null);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      console.log(chosen);
+    }
+    loadAll();
+  }, []);
+
+  return (
+    <div className="Page">
+
+      {FeaturedData &&
+        <FeaturedMovie item={FeaturedData} />
+      }
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
