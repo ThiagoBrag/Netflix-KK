343285d5e069f5535f38399d0c92a08c63a5347b
ThiagoBrag <92130098+ThiagoBrag@users.noreply.github.com>
2022-05-24 18:37:14 -0300

    Netflix
    desccriptions

--- copianetflix/src/components/featuredMovie.js
+++ copianetflix/src/components/featuredMovie.js
@@ -16,20 +16,20 @@ export default ({ item }) => {
                 backgroundposition: 'center',
                 backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
             }}>
-                <div className="fatured--vertical">
-                    <div className="fatured--horizontal">
-                        <div className="fatured--name">{item.original_name}</div>
-                        <div className="fatured--info">
-                            <div className="fatured--points">{item.vote_avarege} pontos</div>
-                            <div className="fatured--year">{firstDate.getFullYear()}</div>
-                            <div className="fatured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                <div className="featured--vertical">
+                    <div className="featured--horizontal">
+                        <div className="featured--name">{item.original_name}</div>
+                        <div className="featured--info">
+                            <div className="featured--points">{item.vote_avarege} pontos</div>
+                            <div className="featured--year">{firstDate.getFullYear()}</div>
+                            <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                         </div>
-                        <div className="fatured--description">{item.overview}</div>
-                        <div className="fatured--buttons">
-                            <a href="">► Assistir</a>
-                            <a href="">+ Minha Lista</a>
+                        <div className="featured--description">{item.overview}</div>
+                        <div className="featured--buttons">
+                            <a href={`/watch/${item.id}`} className="featured--watchbutton">► Assistir</a>
+                            <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
                         </div>
-                        <div className="fatured--genres"><strong>Gêneros: </strong>{genres.join(',')}</div>
+                        <div className="featured--genres"><strong>Gêneros: </strong>{genres.join(',')}</div>
                     </div>
                 </div>
             </section>
